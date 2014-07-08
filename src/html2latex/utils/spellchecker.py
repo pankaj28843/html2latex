e63b68adb1f4c1d972b1cae970be91f1d68f72d3
Pankaj Singh <psjinx@gmail.com>
2014-07-08 11:30:03 +0530

    Trying to make `utils` module more modular.

---
+++ src/html2latex/utils/spellchecker.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import re
+
+import enchant
+from html2text import html2text
+
+DEFAULT_LANGUAGE = "en_UK"
+REGEX_FIND_ENGLISH_WORDS = re.compile(r"[a-zA-Z]+")
+
+
+def get_word_checker(language=DEFAULT_LANGUAGE):
+    """
+    Returns a functions which will return True if word is not
+    present in dictionary.
+    """
+    enchant_dictionary = enchant.Dict(language)
+    spell_checker = lambda w: not enchant_dictionary.check(w)
+    return spell_checker
+
+
+def find_incorrect_words(text, spell_checker):
+    """
+    Finds all english words in the text and returns incorrect words.
+    """
+    words = set(REGEX_FIND_ENGLISH_WORDS.findall(text))
+    return filter(spell_checker, words)
+
+
+def check_spelling(text, language=DEFAULT_LANGUAGE):
+    '''
+    Runs spellcheck and highlights incorrect words using LaTex.
+    '''
+    spell_checker = get_word_checker(language)
+    incorrect_words = find_incorrect_words(text, spell_checker)
+
+    for word in incorrect_words:
+        text = text.replace(
+            word, r"\textcolor{red}{\Large \textbf{" + word + "}}")
+    return text
+
+
+def check_spelling_in_html(html, language=DEFAULT_LANGUAGE):
+    '''
+    - Converts html to text.
+    - Filter incorrect words and highlight them using appropriate HTML.
+    '''
+    spell_checker = get_word_checker(language)
+
+    text = html2text(html)
+    incorrect_words = find_incorrect_words(text, spell_checker)
+
+    for word in incorrect_words:
+        html = html.replace(
+            word, r'<strong style="color: red; font-size: 14px;">' + word + '</strong>')
+    return html
