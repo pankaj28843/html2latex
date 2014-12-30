0018fdae0e8bc2a42da290821b557d6e897ee8aa
Pankaj Singh <psjinx@gmail.com>
2014-12-30 16:16:33 +0530

    Upgraded version number
    Signed-off-by: Pankaj Singh <psjinx@gmail.com>

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ class CustomInstallCommand(InstallCommand):
 
 setup(
     name="html2latex",
-    version="0.0.3",
+    version="0.0.4",
     author="Pankaj Singh",
     author_email="pankaj@policyinnovations.in",
     description=("Convert HTML to latex."),
