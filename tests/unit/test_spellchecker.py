0ee3a54b5cefb9f60542b16d3334e397bdcfb771
Pankaj Kumar Singh <pankaj28843@gmail.com>
2026-01-21 16:07:28 +0100

    MOD-11: migrate tests to pytest

---
+++ tests/unit/test_spellchecker.py
@@ -0,0 +1,43 @@
+import pytest
+
+from html2latex.utils import spellchecker
+
+pytest.importorskip("enchant")
+
+html = """
+<html>
+<body>
+
+<h1>This is headin 1</h1>
+
+
+</body>
+</html>
+"""
+
+
+def test_get_word_checker():
+    checker = spellchecker.get_word_checker("en_US")
+    assert checker("wr000nggg") is True
+    assert checker("correct") is False
+
+def test_find_incorrect_words():
+    wrong_text = "sume increct txt"
+    checker = spellchecker.get_word_checker()
+    wrong_words = set(spellchecker.find_incorrect_words(wrong_text, checker))
+    assert wrong_words == {"increct", "sume", "txt"}
+
+def test_check_spelling():
+    wrong_text = "sum increct wurds"
+    expected_output = (
+        "sum \\textcolor{red}{\\Large \\textbf{increct}} "
+        "\\textcolor{red}{\\Large \\textbf{wurds}}"
+    )
+    assert spellchecker.check_spelling(wrong_text) == expected_output
+
+def test_check_spelling_in_html():
+    output = (
+        "\n<html>\n<body>\n\n<h1>This is"
+        '<strong style="color: red; font-size: 14px;">headin</strong>1</h1>\n\n\n</body>\n</html>\n'
+    )
+    assert spellchecker.check_spelling_in_html(html) == output
---
+++ tests/unit/test_text1.py
@@ -0,0 +1,63 @@
+import pytest
+
+from html2latex.utils import text
+
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("sad\u00c2", "sad "),
+        ("sadas\u00c2", "sadas "),
+        ("Â Goal!!", "  Goal!!"),
+        ("â Goal!!", " Goal!!"),
+        ("â Goal!", "' Goal!"),
+        ("â Super", " Super"),
+        ("â Super!", " Super!"),
+    ],
+)
+def test_clean(value, expected):
+    assert text.clean(value) == expected
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("<p>he scored a goal  &nbsp; </p>", "<p>he scored a goal</p>"),
+        ("<p>What a Goal!!<br></p>", "<p>What a Goal!!</p>"),
+        ("<p>Goal!!! &nbsp;</p><br>", "<p>Goal!!!</p><br>"),
+        ("<br><p>Goal!!! &nbsp;</p><br>", "<br><p>Goal!!! &nbsp;</p><br>"),
+    ],
+)
+def test_clean_paragraph_ending(value, expected):
+    assert text.clean_paragraph_ending(value) == expected
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("Working Function,Create new function.", "Working Function, Create new function. "),
+        ("12213,21.12", "12213,21.12"),
+        ("12213a,21.12", "12213a, 21.12"),
+        (
+            "for football .4 meter is a good measure.So,is this one.",
+            "for football. 4 meter is a good measure. So, is this one. ",
+        ),
+    ],
+)
+def test_fix_text(value, expected):
+    assert text.fix_text(value) == expected
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("Working   : Function", "Working: Function"),
+        ("Working   ; Function;", "Working; Function; "),
+        ("Who is SRT  ?    ", "Who is SRT? "),
+        ("TM scored > 2 goals", "TM scored \\textgreater  2 goals"),
+        ("TM scored < 2 goals", "TM scored \\textless  2 goals"),
+        ("TM scored &gt; 2 goals", "TM scored \\textgreater  2 goals"),
+        ("TM scored &lt; 2 goals", "TM scored \\textless  2 goals"),
+        (" TM'            scored &gt; 2 goals ' ", " TM `scored \\textgreater  2 goals' "),
+        (" TM scored  2 goals \\par ", " TM scored  2 goals "),
+    ],
+)
+def test_fix_formatting(value, expected):
+    assert text.fix_formatting(value) == expected
